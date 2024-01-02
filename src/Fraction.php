313b0dcd6ce8d5712cac06e1f041022eaa8bfb08
Maxime22 <maxime.sabouret@evaneos.com>
2024-01-02 15:07:56 +0100

    add tests for numerator and denominator + signs

--- src/Fraction.php
+++ src/Fraction.php
@@ -4,12 +4,18 @@ namespace App;
 
 readonly class Fraction
 {
-    public function __construct(private int $numerator)
+    public function __construct(private int $numerator, private ?int $denominator = null)
     {
     }
 
-    public function getValue() : int
+    public function getValue() : string
     {
+        if($this->denominator){
+            if($this->denominator<0){
+                return -$this->numerator."/".$this->denominator*-1;
+            }
+            return $this->numerator."/".$this->denominator;
+        }
         return $this->numerator;
     }
 }
--- tests/FirstTest.php
+++ tests/FirstTest.php
@@ -5,8 +5,6 @@ use PHPUnit\Framework\TestCase;
 
 class FirstTest extends TestCase
 {
-//    private Fraction $sut;
-
 
     /**
      * @test
@@ -21,11 +19,84 @@ class FirstTest extends TestCase
         $result = $fraction->getValue();
 
         // THEN
-        self::assertSame($number, $result);
+        self::assertEquals($number, $result);
     }
 
     public static function integerNumberPositiveProvider(): array
     {
         return array_map(fn($number) => [$number], range(1, 3));
     }
+
+    /**
+     * @test
+     */
+    public function fraction_with_a_numerator_and_a_denominator_should_return_both(): void
+    {
+        // GIVEN
+        $numerator= 3;
+        $denominator= 4;
+        $fraction = new Fraction($numerator,$denominator);
+
+        // WHEN
+        $result = $fraction->getValue();
+
+        // THEN
+        $expectedResult = "3/4";
+        self::assertSame($expectedResult, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function fraction_with_negative_numerator_and_positive_denominator_should_return_negative_result(): void
+    {
+        // GIVEN
+        $numerator= -3;
+        $denominator= 4;
+        $fraction = new Fraction($numerator,$denominator);
+
+        // WHEN
+        $result = $fraction->getValue();
+
+        // THEN
+        $expectedResult = "-3/4";
+        self::assertSame($expectedResult, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function fraction_with_positive_numerator_and_negative_denominator_should_return_negative_result(): void
+    {
+        // GIVEN
+        $numerator= 3;
+        $denominator= -4;
+        $fraction = new Fraction($numerator,$denominator);
+
+        // WHEN
+        $result = $fraction->getValue();
+
+        // THEN
+        $expectedResult = "-3/4";
+        self::assertSame($expectedResult, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function fraction_with_negative_numerator_and_negative_denominator_should_return_positive_result(): void
+    {
+        // GIVEN
+        $numerator= -3;
+        $denominator= -4;
+        $fraction = new Fraction($numerator,$denominator);
+
+        // WHEN
+        $result = $fraction->getValue();
+
+        // THEN
+        $expectedResult = "3/4";
+        self::assertSame($expectedResult, $result);
+    }
+
 }
