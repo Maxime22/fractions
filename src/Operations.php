3e913eca42e1ba0eea277f0666fb255a99fe1b74
alicia <alicembru15@hotmail.com>
2024-01-03 09:21:31 +0100

    test: add addition operation with same denominator test

--- src/Operations.php
+++ src/Operations.php
@@ -4,5 +4,13 @@ namespace App;
 
 class Operations
 {
+    public function add(Fraction $fractionA, Fraction $fractionB): string
+    {
 
+        $numeratorResult =  $fractionA->getNumerator() + $fractionB->getNumerator();
+        $denominatorResult = $fractionA->getDenominator();
+
+        $result = new Fraction($numeratorResult, $denominatorResult);
+        return $result->getFractionValue();
+    }
 }
