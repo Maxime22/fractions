ed50d5c25c62cf8b3d93d611cab9d5de5876b7cf
alicia <alicembru15@hotmail.com>
2024-01-03 12:04:50 +0100

    refactor(Operations): extract caculate function

--- src/Operations.php
+++ src/Operations.php
@@ -6,20 +6,20 @@ class Operations
 {
     public function add(Fraction $fractionA, Fraction $fractionB): string
     {
-        $numeratorResult = ($fractionA->getNumerator() * $fractionB->getDenominator()) + ($fractionB->getNumerator() * $fractionA->getDenominator());
+        $numeratorResult = ($fractionA->getNumerator() * $fractionB->getDenominator()) + ($fractionB->getNumerator(
+                ) * $fractionA->getDenominator());
         $denominatorResult = $fractionA->getDenominator() * $fractionB->getDenominator();
 
-        $simplifiedResult = new Fraction($numeratorResult, $denominatorResult);
-        return $simplifiedResult->getFractionValue();
+        return $this->calculateResult($numeratorResult, $denominatorResult);
     }
 
     public function subtract(Fraction $fractionA, Fraction $fractionB): string
     {
-        $numeratorResult = ($fractionA->getNumerator() * $fractionB->getDenominator()) - ($fractionB->getNumerator() * $fractionA->getDenominator());
+        $numeratorResult = ($fractionA->getNumerator() * $fractionB->getDenominator()) - ($fractionB->getNumerator(
+                ) * $fractionA->getDenominator());
         $denominatorResult = $fractionA->getDenominator() * $fractionB->getDenominator();
 
-        $simplifiedResult = new Fraction($numeratorResult, $denominatorResult);
-        return $simplifiedResult->getFractionValue();
+        return $this->calculateResult($numeratorResult, $denominatorResult);
     }
 
     public function multiply(Fraction $fractionA, Fraction $fractionB): string
@@ -27,8 +27,7 @@ class Operations
         $numeratorResult = $fractionA->getNumerator() * $fractionB->getNumerator();
         $denominatorResult = $fractionA->getDenominator() * $fractionB->getDenominator();
 
-        $simplifiedResult = new Fraction($numeratorResult, $denominatorResult);
-        return $simplifiedResult->getFractionValue();
+        return $this->calculateResult($numeratorResult, $denominatorResult);
     }
 
     public function divide(Fraction $fractionA, Fraction $fractionB): string
@@ -36,6 +35,11 @@ class Operations
         $numeratorResult = $fractionA->getNumerator() * $fractionB->getDenominator();
         $denominatorResult = $fractionA->getDenominator() * $fractionB->getNumerator();
 
+        return $this->calculateResult($numeratorResult, $denominatorResult);
+    }
+
+    private function calculateResult(int $numeratorResult, int $denominatorResult): string
+    {
         $simplifiedResult = new Fraction($numeratorResult, $denominatorResult);
         return $simplifiedResult->getFractionValue();
     }
